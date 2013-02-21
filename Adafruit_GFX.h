01273c2ad20fc9c7c661b8f882883023edd7afdb
schlammbad <schlammbad@hotmail.com>
2013-02-20 19:41:00 -0800

    New overload for drawBitmap to support background color.

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -66,6 +66,9 @@ class Adafruit_GFX : public Print {
   void drawBitmap(int16_t x, int16_t y, 
 		  const uint8_t *bitmap, int16_t w, int16_t h,
 		  uint16_t color);
+  void drawBitmap(int16_t x, int16_t y,
+		  const uint8_t *bitmap, int16_t x, int16_t h,
+		  uint16_t color, uint16_t bg);
   void drawChar(int16_t x, int16_t y, unsigned char c,
 		uint16_t color, uint16_t bg, uint8_t size);
 #if ARDUINO >= 100
