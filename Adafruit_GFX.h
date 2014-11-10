25780006316dde525820156d911f6c14fc70a8fc
Calvin Beck <hobbes@ualberta.ca>
2014-11-10 02:43:42 -0700

    Made swap a function to prevent awful error messages from macros.

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -8,7 +8,7 @@
  #include "WProgram.h"
 #endif
 
-#define swap(a, b) { int16_t t = a; a = b; b = t; }
+inline void swap(int16_t &a, int16_t &b) { int16_t t = a; a = b; b = t; }
 
 class Adafruit_GFX : public Print {
 
