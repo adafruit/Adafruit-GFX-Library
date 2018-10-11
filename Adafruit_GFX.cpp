5fcf5e7b340a1bd097dad38be37b8f712ffab4f7
winneymj <winneymj@gmail.com>
2018-10-10 21:27:29 -0500

    Handle inverse custom font by not writing to the background outside the xAdvance as this is the true width of the character.

--- Adafruit_GFX.cpp
+++ Adafruit_GFX.cpp
@@ -1065,6 +1065,8 @@ void Adafruit_GFX::drawChar(int16_t x, int16_t y, unsigned char c,
                  h  = pgm_read_byte(&glyph->height);
         int8_t   xo = pgm_read_byte(&glyph->xOffset),
                  yo = pgm_read_byte(&glyph->yOffset);
+        int8_t   xa = pgm_read_byte(&glyph->xAdvance);
+
         uint8_t  xx, yy, bits = 0, bit = 0;
         int16_t  xo16 = 0, yo16 = 0;
 
@@ -1104,6 +1106,16 @@ void Adafruit_GFX::drawChar(int16_t x, int16_t y, unsigned char c,
                         writeFillRect(x+(xo16+xx)*size, y+(yo16+yy)*size,
                           size, size, color);
                     }
+                } else if(bg != color) {
+					// Only write pixels inside the xadvance as this is the true width of the font.
+					if (xx < xa) {
+						if(size == 1) {
+							writePixel(x+xo+xx, y+yo+yy, bg);
+						} else {
+							writeFillRect(x+(xo16+xx)*size, y+(yo16+yy)*size,
+							  size, size, bg);
+						}
+					}
                 }
                 bits <<= 1;
             }
