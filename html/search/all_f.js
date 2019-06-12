c52dc0196a44c98a25448c9deefcea0ccebf20a2
Travis CI <travis@travis-ci.org>
2019-06-12 20:55:24 +0000

    Deploy code docs to GitHub Pages Travis build: 226
    Commit: 306f95ad1dad71677f7e37df3ab9313f93479e01

--- html/search/all_f.js
+++ html/search/all_f.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['textbgcolor',['textbgcolor',['../class_adafruit___g_f_x.html#a23e7a4efcab0b1588dc0cafa14b1fac1',1,'Adafruit_GFX']]],
   ['textcolor',['textcolor',['../class_adafruit___g_f_x.html#a8c6d23a386651136fd9530a5b7046591',1,'Adafruit_GFX']]],
-  ['textsize',['textsize',['../class_adafruit___g_f_x.html#ac293848b8fe8c46107d1a491f6a5168d',1,'Adafruit_GFX']]],
+  ['textsize_5fx',['textsize_x',['../class_adafruit___g_f_x.html#a9c33cd6928ad6738a633e97727ae8773',1,'Adafruit_GFX']]],
+  ['textsize_5fy',['textsize_y',['../class_adafruit___g_f_x.html#aa8588c3941eec083563c853ada3dd771',1,'Adafruit_GFX']]],
   ['tft8',['tft8',['../class_adafruit___s_p_i_t_f_t.html#a76e31d8fcece00ed7bb2950efc5270e2',1,'Adafruit_SPITFT']]],
   ['tft_5fhard_5fspi',['TFT_HARD_SPI',['../_adafruit___s_p_i_t_f_t_8cpp.html#af52f9514c56a91e627a10fea9ffc6847',1,'Adafruit_SPITFT.cpp']]],
   ['tft_5fparallel',['TFT_PARALLEL',['../_adafruit___s_p_i_t_f_t_8cpp.html#a8c9abc3b5a2981921143a9b6be13bf90',1,'Adafruit_SPITFT.cpp']]],
