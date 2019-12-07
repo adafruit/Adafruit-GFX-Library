579db8f4a71b7063055b957f68d20e3802791ec2
Travis CI <travis@travis-ci.org>
2019-12-07 23:35:27 +0000

    Deploy code docs to GitHub Pages Travis build: 273
    Commit: 18066959f4decdf15514d30373fd676a8fd4aa1f

--- html/search/functions_b.js
+++ html/search/functions_b.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['read16',['read16',['../class_adafruit___s_p_i_t_f_t.html#ae5ded96bb6051c6f882dfc6bac4557bc',1,'Adafruit_SPITFT']]],
+  ['readcommand16',['readcommand16',['../class_adafruit___s_p_i_t_f_t.html#ac80f648cc0abc7b1d1ec27a750e5f79a',1,'Adafruit_SPITFT']]],
   ['readcommand8',['readcommand8',['../class_adafruit___s_p_i_t_f_t.html#a641ccc4f159129259ee08738cb99627b',1,'Adafruit_SPITFT']]]
 ];
