70950e3d3b71f21582f409551334c3377fd19171
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-12-19 11:20:06 -0800

    Added removed sendCommand overload to fix rotation

--- Adafruit_SPITFT.h
+++ Adafruit_SPITFT.h
@@ -194,6 +194,7 @@ class Adafruit_SPITFT : public Adafruit_GFX {
     void         startWrite(void);
     // Chip deselect and/or hardware SPI transaction end as needed:
     void         endWrite(void);
+    void         sendCommand(uint8_t commandByte, uint8_t *dataBytes, uint8_t numDataBytes);
     void         sendCommand(uint8_t commandByte, const uint8_t *dataBytes = NULL, uint8_t numDataBytes = 0);
     void         sendCommand16(uint16_t commandWord, const uint8_t *dataBytes = NULL, uint8_t numDataBytes = 0);
     uint8_t      readcommand8(uint8_t commandByte, uint8_t index = 0);
