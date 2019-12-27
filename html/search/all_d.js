62b0fad17dbca98d07544adbc268cb3dc1dcb664
Doxygen CI <travis@travis-ci.org>
2019-12-27 20:06:08 +0000

    Deploy code docs to GitHub Pages Travis build: 292
    Commit: d1d5cdaa27485d39e2a93ee141458a621d1c8d3b

--- html/search/all_d.js
+++ html/search/all_d.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['read16',['read16',['../class_adafruit___s_p_i_t_f_t.html#ae5ded96bb6051c6f882dfc6bac4557bc',1,'Adafruit_SPITFT']]],
-  ['readcommand16',['readcommand16',['../class_adafruit___s_p_i_t_f_t.html#ac80f648cc0abc7b1d1ec27a750e5f79a',1,'Adafruit_SPITFT']]],
   ['readcommand8',['readcommand8',['../class_adafruit___s_p_i_t_f_t.html#a641ccc4f159129259ee08738cb99627b',1,'Adafruit_SPITFT']]],
   ['rotation',['rotation',['../class_adafruit___g_f_x.html#a37a479d28fb11906ce516e983b1af926',1,'Adafruit_GFX']]]
 ];
