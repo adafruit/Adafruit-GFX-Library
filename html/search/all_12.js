d156f8630399a6291dbe7b5f577497a5c7b8d85e
Doxygen CI <travis@travis-ci.org>
2020-05-14 22:58:11 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_12.js
+++ html/search/all_12.js
@@ -2,6 +2,10 @@ var searchData=
 [
   ['wide',['wide',['../class_adafruit___s_p_i_t_f_t.html#ab49cda19025e4f18f65f1a00ce65693d',1,'Adafruit_SPITFT']]],
   ['width',['WIDTH',['../class_adafruit___g_f_x.html#aded338777abeac3c457660023f5c0724',1,'Adafruit_GFX::WIDTH()'],['../struct_g_f_xglyph.html#a02bd34fd474af250b230f52cee75a6c1',1,'GFXglyph::width()'],['../class_adafruit___g_f_x.html#a324b5361e7198ef0e79eaf4c80bddfc7',1,'Adafruit_GFX::width()']]],
+  ['window_5fx1',['window_x1',['../class_adafruit___mono_o_l_e_d.html#a84fa64243a1985b73f4895775268f5c2',1,'Adafruit_MonoOLED']]],
+  ['window_5fx2',['window_x2',['../class_adafruit___mono_o_l_e_d.html#a0313339d1718fb3fdb6f48d9a8b9a058',1,'Adafruit_MonoOLED']]],
+  ['window_5fy1',['window_y1',['../class_adafruit___mono_o_l_e_d.html#a404bc5e3ee3d2cb19272d570edc91ec4',1,'Adafruit_MonoOLED']]],
+  ['window_5fy2',['window_y2',['../class_adafruit___mono_o_l_e_d.html#a978c850a8d75edc1f7609376a6bd9f92',1,'Adafruit_MonoOLED']]],
   ['wrap',['wrap',['../class_adafruit___g_f_x.html#ad6bd603e01861212829d536312a7190b',1,'Adafruit_GFX']]],
   ['write',['write',['../class_adafruit___g_f_x.html#a8773d92cafa93d3f749fb55c535d8f2e',1,'Adafruit_GFX']]],
   ['write16',['write16',['../class_adafruit___s_p_i_t_f_t.html#adef142a386721ffc13b5efbd194d0e69',1,'Adafruit_SPITFT']]],
