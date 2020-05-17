f7aef786cf40f9ea30f9dc2bae9619df7ee03575
Doxygen CI <travis@travis-ci.org>
2020-05-17 16:39:08 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['charbounds',['charBounds',['../class_adafruit___g_f_x.html#a21c19ed786f8291613d082e81ad4e3c4',1,'Adafruit_GFX']]],
+  ['charbounds',['charBounds',['../class_adafruit___g_f_x.html#a682fb355070668dba526be20a9ce45af',1,'Adafruit_GFX']]],
   ['cleardisplay',['clearDisplay',['../class_adafruit___mono_o_l_e_d.html#a9a4d2370e3a4b7d9189d913e617edfcc',1,'Adafruit_MonoOLED']]],
   ['color565',['color565',['../class_adafruit___s_p_i_t_f_t.html#a656f04e5a60f1ff7cddc2934fa676b8b',1,'Adafruit_SPITFT']]],
   ['connection',['connection',['../class_adafruit___s_p_i_t_f_t.html#ae17cc84566e35b026f8b6f38903be80b',1,'Adafruit_SPITFT']]],
--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['charbounds',['charBounds',['../class_adafruit___g_f_x.html#a21c19ed786f8291613d082e81ad4e3c4',1,'Adafruit_GFX']]],
+  ['charbounds',['charBounds',['../class_adafruit___g_f_x.html#a682fb355070668dba526be20a9ce45af',1,'Adafruit_GFX']]],
   ['cleardisplay',['clearDisplay',['../class_adafruit___mono_o_l_e_d.html#a9a4d2370e3a4b7d9189d913e617edfcc',1,'Adafruit_MonoOLED']]],
   ['color565',['color565',['../class_adafruit___s_p_i_t_f_t.html#a656f04e5a60f1ff7cddc2934fa676b8b',1,'Adafruit_SPITFT']]],
   ['contains',['contains',['../class_adafruit___g_f_x___button.html#aa5fb594cf6f9cd4c1815a7c1011b5830',1,'Adafruit_GFX_Button']]],
