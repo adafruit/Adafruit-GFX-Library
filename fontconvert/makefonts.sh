ae10749794a3a256f06a0d86ef22d7420442fc21
Billy Donahue <billy.donahue@gmail.com>
2020-05-29 17:05:51 -0400

    revamp scripting

--- fontconvert/makefonts.sh
+++ fontconvert/makefonts.sh
@@ -12,27 +12,24 @@
 # (huge zipfile, different license) but they're easily acquired:
 # http://savannah.gnu.org/projects/freefont/
 
-convert=./fontconvert
-inpath=~/Desktop/freefont/
-outpath=../Fonts/
+convert=${convert:-./fontconvert}
+inpath=${inpath:-~/Desktop/freefont}
+outpath=${outpath:-../Fonts}
+
 fonts=(FreeMono FreeSans FreeSerif)
 styles=("" Bold Italic BoldItalic Oblique BoldOblique)
-sizes=(9 12 18 24)
+#sizes=(9 12 18 24)
 
-for f in ${fonts[*]}
-do
-	for index in ${!styles[*]}
-	do
-		st=${styles[$index]}
-		for si in ${sizes[*]}
-		do
-			infile=$inpath$f$st".ttf"
-			if [ -f $infile ] # Does source combination exist?
-			  then
-				outfile=$outpath$f$st$si"pt7b.h"
-#				printf "%s %s %s > %s\n" $convert $infile $si $outfile
-				$convert $infile $si > $outfile
-			fi
-		done
-	done
+for f in "${fonts[@]}"; do
+    for st in "${styles[@]}"; do
+        infile="${inpath}/${f}${st}.ttf"
+        if [[ ! -f $infile ]] ; then
+          echo "no input file: \"$infile\"" >&2
+          continue
+        fi
+        for si in 9 12 18 24 ; do
+          outfile="${outpath}/${f}${st}${si}pt7b.h"
+          ( set -x ; "$convert" "$infile" "$si" > "$outfile" )
+        done
+    done
 done
