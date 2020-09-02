92e4d42e6b1e09ef295987b6b76f8a7094b8d4ba
Doxygen CI <travis@travis-ci.org>
2020-09-02 15:32:34 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_12.js
+++ html/search/all_12.js
@@ -12,7 +12,7 @@ var searchData=
   ['writecolor',['writeColor',['../class_adafruit___s_p_i_t_f_t.html#a42824e0f1475f2b3df335d31651dc75a',1,'Adafruit_SPITFT']]],
   ['writecommand',['writeCommand',['../class_adafruit___s_p_i_t_f_t.html#a0169fcf8898413bba568a93c1617e8bb',1,'Adafruit_SPITFT']]],
   ['writecommand16',['writeCommand16',['../class_adafruit___s_p_i_t_f_t.html#aa327bf9bd8580fdc87018df917259c97',1,'Adafruit_SPITFT']]],
-  ['writefasthline',['writeFastHLine',['../class_adafruit___g_f_x.html#ad85469e8be73f90932058fba1d4e5cbd',1,'Adafruit_GFX::writeFastHLine()'],['../class_g_f_xcanvas8.html#a5eadf69798e020345107af6cdb86f222',1,'GFXcanvas8::writeFastHLine()'],['../class_adafruit___s_p_i_t_f_t.html#a06312f906256fd7e72efb21df85d2b96',1,'Adafruit_SPITFT::writeFastHLine()']]],
+  ['writefasthline',['writeFastHLine',['../class_adafruit___g_f_x.html#ad85469e8be73f90932058fba1d4e5cbd',1,'Adafruit_GFX::writeFastHLine()'],['../class_adafruit___s_p_i_t_f_t.html#a06312f906256fd7e72efb21df85d2b96',1,'Adafruit_SPITFT::writeFastHLine()']]],
   ['writefastvline',['writeFastVLine',['../class_adafruit___g_f_x.html#ae12d09670433af17e666a203b51715f7',1,'Adafruit_GFX::writeFastVLine()'],['../class_adafruit___s_p_i_t_f_t.html#a9b5f5765414c6b0c94c9a060427803ec',1,'Adafruit_SPITFT::writeFastVLine()']]],
   ['writefillrect',['writeFillRect',['../class_adafruit___g_f_x.html#a60133faeabc10f9d909674bc4c5aabc6',1,'Adafruit_GFX::writeFillRect()'],['../class_adafruit___s_p_i_t_f_t.html#a8df7d60b7c8500b31d0a8f59999c3df1',1,'Adafruit_SPITFT::writeFillRect()']]],
   ['writefillrectpreclipped',['writeFillRectPreclipped',['../class_adafruit___s_p_i_t_f_t.html#a6ef20b186a133d416f9ba71fb884d275',1,'Adafruit_SPITFT']]],
