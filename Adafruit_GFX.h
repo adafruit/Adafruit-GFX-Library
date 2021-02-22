374fa114d57a2297b1a9b2240968b4d26f079d84
J <jeremy@zunkworks.com>
2021-02-22 16:45:28 -0600

    Add function overload for drawXBitmap to allow drawing a background color

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -88,6 +88,8 @@ public:
                   uint16_t color, uint16_t bg);
   void drawXBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w,
                    int16_t h, uint16_t color);
+  void drawXBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w,
+                   int16_t h, uint16_t color, uint16_t bg);
   void drawGrayscaleBitmap(int16_t x, int16_t y, const uint8_t bitmap[],
                            int16_t w, int16_t h);
   void drawGrayscaleBitmap(int16_t x, int16_t y, uint8_t *bitmap, int16_t w,
