5ae86038e14d63af3e7d1c748321eb5d667d9e80
IanBUK <78267962+IanBUK@users.noreply.github.com>
2021-04-19 08:02:53 +0100

    Formatting fixes based on clang

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -261,13 +261,12 @@ public:
                   uint8_t textsize_y);				  
   void initButton(Adafruit_GFX *gfx, int16_t x, int16_t y, uint16_t w,
                   uint16_t h, uint16_t outline, uint16_t fill,
-                  uint16_t textcolor, uint16_t pressedcolor, char *label, 
-		  uint8_t textsize);
-				  
+                  uint16_t textcolor, uint16_t pressedcolor, char *label,
+                  uint8_t textsize);		  
   void initButton(Adafruit_GFX *gfx, int16_t x, int16_t y, uint16_t w,
                   uint16_t h, uint16_t outline, uint16_t fill,
-                  uint16_t textcolor, uint16_t pressedcolor, char *label, 
-		  uint8_t textsize_x, uint8_t textsize_y);
+                  uint16_t textcolor, uint16_t pressedcolor, char *label,
+                  uint8_t textsize_x, uint8_t textsize_y);
   // New/alt initButton() uses upper-left corner & size
   void initButtonUL(Adafruit_GFX *gfx, int16_t x1, int16_t y1, uint16_t w,
                     uint16_t h, uint16_t outline, uint16_t fill,
@@ -276,14 +275,13 @@ public:
                     uint16_t h, uint16_t outline, uint16_t fill,
                     uint16_t textcolor, char *label, uint8_t textsize_x,
                     uint8_t textsize_y);
-  // New/alt initButton() uses upper-left corner & size
   void initButtonUL(Adafruit_GFX *gfx, int16_t x1, int16_t y1, uint16_t w,
                     uint16_t h, uint16_t outline, uint16_t fill,
-                    uint16_t textcolor, uint16_t pressedcolor, char *label, 
+                    uint16_t textcolor, uint16_t pressedcolor, char *label,
 		    uint8_t textsize);
   void initButtonUL(Adafruit_GFX *gfx, int16_t x1, int16_t y1, uint16_t w,
                     uint16_t h, uint16_t outline, uint16_t fill,
-                    uint16_t textcolor, uint16_t pressedcolor, char *label, 
+                    uint16_t textcolor, uint16_t pressedcolor, char *label,
 		    uint8_t textsize_x, uint8_t textsize_y);
 					
   void drawButton(bool inverted = false);
@@ -309,7 +307,7 @@ public:
     @returns  True if pressed
   */
   /**********************************************************************/
-  bool isPressed(void) { return currstate; };  
+  bool isPressed(void) { return currstate; };
   /**********************************************************************/
   /*!
     @brief    Get the label text for the button
@@ -317,9 +315,9 @@ public:
   */
   /**********************************************************************/
   const char *getLabel() const { return _label; };
-  
+
   int radius = 4;
-  
+
 private:
   Adafruit_GFX *_gfx;
   int16_t _x1, _y1; // Coordinates of top-left corner
@@ -327,7 +325,7 @@ private:
   uint8_t _textsize_x;
   uint8_t _textsize_y;
   uint16_t _outlinecolor, _fillcolor, _textcolor, _pressedcolor;
-  
+
   static const int _maxlabellength = 40;
   char _label[_maxlabellength];
 
