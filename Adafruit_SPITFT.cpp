8d14ea77ac245d058988fd298fd36ef724781e37
Alexander Golovanov <45916903+homeodor@users.noreply.github.com>
2022-04-27 18:43:27 +0300

    Fixed clang-format

--- Adafruit_SPITFT.cpp
+++ Adafruit_SPITFT.cpp
@@ -1175,8 +1175,7 @@ void Adafruit_SPITFT::dmaWait(void) {
     @brief  Check if DMA transfer is active. Always returts false  if DMA
             is not enabled.
 */
-bool Adafruit_SPITFT::dmaBusy(void) const
-{
+bool Adafruit_SPITFT::dmaBusy(void) const {
 #if defined(USE_SPI_DMA) && (defined(__SAMD51__) || defined(ARDUINO_SAMD_ZERO))
   return dma_busy;
 #else
