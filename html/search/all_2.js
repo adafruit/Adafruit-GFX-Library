5cd19f94af60f956d8b42d6ceddedf0583e8abc2
Doxygen CI <ci-arduino@invalid>
2023-01-16 23:22:55 +0000

    Deploy docs to GitHub Pages from commit e122c3d20f
    Commit: e122c3d20f5ee6baf366d9b72d162ba6d0a133b0
    GitHub Actions run: 3934635248

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -3,6 +3,6 @@ var searchData=
   ['begin',['begin',['../class_adafruit___s_p_i_t_f_t.html#a81fb1a7842c52fa1992569880f4ea494',1,'Adafruit_SPITFT']]],
   ['bitmap',['bitmap',['../struct_g_f_xfont.html#a867b14d604450addbeaf62ae1e4799a7',1,'GFXfont']]],
   ['bitmapoffset',['bitmapOffset',['../struct_g_f_xglyph.html#a24d52036821c10af52a742a1d7fbd9b8',1,'GFXglyph']]],
-  ['buffer',['buffer',['../class_adafruit___gray_o_l_e_d.html#af554594766a360e1db431ee7e4e4b179',1,'Adafruit_GrayOLED']]],
+  ['buffer',['buffer',['../class_g_f_xcanvas1.html#aec1b5879f2dd91b7a13da12fe48ae5e7',1,'GFXcanvas1::buffer()'],['../class_g_f_xcanvas8.html#a9798c30f72aacd8a1488368375b0352f',1,'GFXcanvas8::buffer()'],['../class_g_f_xcanvas16.html#a13e81bad95bdb3a36d24d2778deb075e',1,'GFXcanvas16::buffer()'],['../class_adafruit___gray_o_l_e_d.html#af554594766a360e1db431ee7e4e4b179',1,'Adafruit_GrayOLED::buffer()']]],
   ['byteswap',['byteSwap',['../class_g_f_xcanvas16.html#a1d0f26a1252ad384612ca14b07cee98e',1,'GFXcanvas16']]]
 ];
--- html/search/variables_1.js
+++ html/search/variables_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['bitmap',['bitmap',['../struct_g_f_xfont.html#a867b14d604450addbeaf62ae1e4799a7',1,'GFXfont']]],
   ['bitmapoffset',['bitmapOffset',['../struct_g_f_xglyph.html#a24d52036821c10af52a742a1d7fbd9b8',1,'GFXglyph']]],
-  ['buffer',['buffer',['../class_adafruit___gray_o_l_e_d.html#af554594766a360e1db431ee7e4e4b179',1,'Adafruit_GrayOLED']]]
+  ['buffer',['buffer',['../class_g_f_xcanvas1.html#aec1b5879f2dd91b7a13da12fe48ae5e7',1,'GFXcanvas1::buffer()'],['../class_g_f_xcanvas8.html#a9798c30f72aacd8a1488368375b0352f',1,'GFXcanvas8::buffer()'],['../class_g_f_xcanvas16.html#a13e81bad95bdb3a36d24d2778deb075e',1,'GFXcanvas16::buffer()'],['../class_adafruit___gray_o_l_e_d.html#af554594766a360e1db431ee7e4e4b179',1,'Adafruit_GrayOLED::buffer()']]]
 ];
