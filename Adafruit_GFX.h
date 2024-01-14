4e3a95f51703e381e94977b7a0bd1e9959255250
nathan <nathanjshaffer@gmail.com>
2024-01-13 21:08:42 -0500

    Update Adafruit_GFX.cpp
    Bugfix
    clang
    
    Document member variables in  Adafruit_GFX_Button
    
    corrected compile arrors
    
    more clang
    
    fixed variable documentation

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -295,13 +295,16 @@ public:
   bool isPressed(void) { return currstate; };
 
 protected:
-  Adafruit_GFX *_gfx;
-  int16_t _x1, _y1; // Coordinates of top-left corner
-  uint16_t _w, _h;
-  uint8_t _textsize_x;
-  uint8_t _textsize_y;
-  uint16_t _outlinecolor, _fillcolor, _textcolor;
-  char _label[10];
+  Adafruit_GFX *_gfx;     ///< gfx display pointer
+  int16_t _x1;            ///< x Coordinate of top-left corner
+  int16_t _y1;            ///< y Coordinate of top-left corner
+  uint16_t _w, _h;        ///< width and heaight of button
+  uint8_t _textsize_x;    ///< width of text character in pixels
+  uint8_t _textsize_y;    ///< height of text character in pixels
+  uint16_t _outlinecolor; ///< button outline color
+  uint16_t _fillcolor;    ///< button fill color
+  uint16_t _textcolor;    ///< text color
+  char _label[10];        ///< text label on button
 
   bool currstate, laststate;
 };
