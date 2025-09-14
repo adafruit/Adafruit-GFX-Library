e2226ebecb7c72d2b000c6af1d643ed8dec3211f
Doxygen CI <ci-arduino@invalid>
2025-09-14 12:32:30 +0000

    Deploy docs to GitHub Pages from commit 32da3d2094
    Commit: 32da3d2094e6c3a6cb0c6996a61f4a42cbf492d5
    GitHub Actions run: 17711160383

--- html/search/variables_c.js
+++ html/search/variables_c.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['wide',['wide',['../class_adafruit___s_p_i_t_f_t.html#ab49cda19025e4f18f65f1a00ce65693d',1,'Adafruit_SPITFT']]],
-  ['width',['WIDTH',['../class_adafruit___g_f_x.html#aded338777abeac3c457660023f5c0724',1,'Adafruit_GFX::WIDTH()'],['../struct_g_f_xglyph.html#a02bd34fd474af250b230f52cee75a6c1',1,'GFXglyph::width()']]],
+  ['width',['width',['../struct_g_f_xglyph.html#a02bd34fd474af250b230f52cee75a6c1',1,'GFXglyph::width()'],['../class_adafruit___g_f_x.html#aded338777abeac3c457660023f5c0724',1,'Adafruit_GFX::WIDTH()']]],
   ['window_5fx1',['window_x1',['../class_adafruit___gray_o_l_e_d.html#a83073a894e500684888a4802d49bdf0d',1,'Adafruit_GrayOLED']]],
   ['window_5fx2',['window_x2',['../class_adafruit___gray_o_l_e_d.html#aaeb1bf335e8d97893d9e8b461fb16a76',1,'Adafruit_GrayOLED']]],
   ['window_5fy1',['window_y1',['../class_adafruit___gray_o_l_e_d.html#ae9ea4d1c3be6899b5e65e7e7554498e8',1,'Adafruit_GrayOLED']]],
