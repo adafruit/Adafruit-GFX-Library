18e6e8bbfdbb431a1806392ed362109c638bbe95
Bill Merryman <billmerryman@gmail.com>
2025-11-07 08:53:44 -0500

    Add quarter circle draw method (formatting)

--- Adafruit_GFX.cpp
+++ Adafruit_GFX.cpp
@@ -638,9 +638,9 @@ void Adafruit_GFX::fillCircleHelper(int16_t x0, int16_t y0, int16_t r,
     @param  color    16-bit 5-6-5 Color to fill with
 */
 /**************************************************************************/
-void Adafruit_GFX::fillQuarterCircle(int16_t x0, int16_t y0,
-                                    int16_t r, uint8_t corners, int16_t delta,
-                                    uint16_t color) {
+void Adafruit_GFX::fillQuarterCircle(int16_t x0, int16_t y0, int16_t r,
+                                     uint8_t corners, int16_t delta,
+                                     uint16_t color) {
 
   int16_t f = 1 - r;
   int16_t ddF_x = 1;
